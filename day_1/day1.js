9b9320464baad5a92880ecead8cf6e0b667e331a
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-03-31 20:06:15 +0530

    Print All Even Numbers from 1 to N

--- day_1/day1.js
+++ day_1/day1.js
@@ -8,8 +8,19 @@
 
 // Print Numbers from N to 1 without changing the loop condition of above question
 
-const n = 5;
+// const n = 5;
 
-for (let i = 1; i <= n; i++){
-    console.log(n - (n - i));
+// for (let i = 1; i <= n; i++){
+//     console.log(n - (n - i));
+// }
+
+
+// Print All Even Numbers from 1 to N
+
+const n = 25;
+
+for (let i = 0; i <= n; i++){
+    if (i % 2 === 0) {
+        console.log(i);
+    }
 }
