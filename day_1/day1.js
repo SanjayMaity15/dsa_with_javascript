b787fc6e04bb880400189779fa8f3bea671d87bb
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-03-31 20:05:04 +0530

    Print Numbers from N to 1 without changing the loop condition of above question

--- day_1/day1.js
+++ day_1/day1.js
@@ -1,6 +1,15 @@
 // Print Numbers from 1 to N
 
+// const n = 5;
+// for (let i = 1; i <= n; i++){
+//     console.log(i);
+// }
+
+
+// Print Numbers from N to 1 without changing the loop condition of above question
+
 const n = 5;
+
 for (let i = 1; i <= n; i++){
-    console.log(i);
+    console.log(n - (n - i));
 }
