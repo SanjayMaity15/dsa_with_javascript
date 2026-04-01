a5dcc68068403be5ea70e29a695bf39c8ad6fc44
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-01 09:57:03 +0530

    Find the Maximum of Three Numbers

---
+++ day_2/day2.js
@@ -0,0 +1,15 @@
+// ! Find the Maximum of Three Numbers
+
+const n1 = 64,
+	n2 = 4,
+	n3 = 24;
+
+if (n1 > n2 && n1 > n3) {
+	console.log("Largest number is ", n1);
+} else if (n2 > n1 && n2 > n3) {
+	console.log("Largest number is ", n2);
+} else if (n3 > n1 && n3 > n2) {
+	console.log("Largest number is ", n3);
+} else {
+    console.log("All are equal");
+}
