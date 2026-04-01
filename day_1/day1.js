65196bed48abcb2e570ba4b16905f3ff76388be4
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-01 06:35:21 +0530

    day 1 completed

--- day_1/day1.js
+++ day_1/day1.js
@@ -1,118 +1,118 @@
 //! Print Numbers from 1 to N
 
-// const n = 5;
-// for (let i = 1; i <= n; i++){
-//     console.log(i);
-// }
+const n = 5;
+for (let i = 1; i <= n; i++){
+    console.log(i);
+}
 
 
-// Print Numbers from N to 1 without changing the loop condition of above question
+//! Print Numbers from N to 1 without changing the loop condition of above question
 
-// const n = 5;
+const n = 5;
 
-// for (let i = 1; i <= n; i++){
-//     console.log(n - (n - i));
-// }
-
-
-// Print All Even Numbers from 1 to N
-
-// const n = 25;
-
-// for (let i = 0; i <= n; i++){
-//     if (i % 2 === 0) {
-//         console.log(i);
-//     }
-// }
+for (let i = 1; i <= n; i++){
+    console.log(n - (n - i));
+}
 
 
-// Sum of First N Natural Numbers
+//! Print All Even Numbers from 1 to N
 
-// const n = 10;
+const n = 25;
 
- // one way
-// console.log((n * (n + 1)) / 2);
-
- // other way
-
-// let sum = 0;
-// for (let i = 1; i <= n; i++){
-//     sum += i;
-// }
-
-// console.log(sum);
+for (let i = 0; i <= n; i++){
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
 
 
-// Product (Factorial) of N
+//! Sum of First N Natural Numbers
 
-// const n = 5;
+const n = 10;
 
-// let fact = 1;
-// for (let i = 1; i <= n; i++){
-//     fact *= i;
-// }
+//  one way
+console.log((n * (n + 1)) / 2);
 
-// console.log(fact);
+//  other way
 
+let sum = 0;
+for (let i = 1; i <= n; i++){
+    sum += i;
+}
 
-// Sum of All Even Numbers up to N
-
-// const n = 20;
-
-// let sum = 0;
-
-// for (let i = 1; i <= n; i++){
-//     if (i % 2 === 0) {
-//         sum += i;
-//     }
-// }
-
-// console.log(sum);
-
-// Print Squares of Numbers from 1 to N
-
-// const n = 10
-
-// for (let i = 1; i <= n; i++){
-//     console.log(i * i);
-// }
+console.log(sum);
 
 
-// Print all numbers divisible by 3 and 5 up to N
+//! Product (Factorial) of N
 
-// const n = 30;
+const n = 5;
 
-// for (let i = 1; i <= n; i++){
-//     if (i % 3 === 0 && i % 5 === 0) {
-//         console.log(i);
-//     }
-// }
+let fact = 1;
+for (let i = 1; i <= n; i++){
+    fact *= i;
+}
 
-
-// Find the sum of all odd numbers up to N
-
-// const n = 10;
-
-// let sum = 0;
-// for (let i = 1; i <= n; i++){
-//     if (i % 2 !== 0) {
-//         sum += i;
-//     }
-// }
-
-// console.log(sum);
+console.log(fact);
 
 
-// Print the cubes of numbers from 1 to N
+//! Sum of All Even Numbers up to N
 
-// const n = 5;
+const n = 20;
 
-// for (let i = 1; i <= n; i++){
-//     console.log(Math.pow(i, 3));
-// }
+let sum = 0;
+
+for (let i = 1; i <= n; i++){
+    if (i % 2 === 0) {
+        sum += i;
+    }
+}
+
+console.log(sum);
+
+//! Print Squares of Numbers from 1 to N
+
+const n = 10
+
+for (let i = 1; i <= n; i++){
+    console.log(i * i);
+}
 
 
-// Print only the numbers that are both even and perfect squares
+//! Print all numbers divisible by 3 and 5 up to N
+
+const n = 30;
+
+for (let i = 1; i <= n; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i);
+    }
+}
+
+
+//! Find the sum of all odd numbers up to N
+
+const n = 10;
+
+let sum = 0;
+for (let i = 1; i <= n; i++){
+    if (i % 2 !== 0) {
+        sum += i;
+    }
+}
+
+console.log(sum);
+
+
+//! Print the cubes of numbers from 1 to N
+
+const n = 5;
+
+for (let i = 1; i <= n; i++){
+    console.log(Math.pow(i, 3));
+}
+
+
+//! Print only the numbers that are both even and perfect squares
 
 const n = 20;
 
