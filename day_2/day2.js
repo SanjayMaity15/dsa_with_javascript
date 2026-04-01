27e96c1cc76fcd7305c3a9028e0f79040199cbbd
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-01 10:13:51 +0530

    Calculate Electricity Bill

--- day_2/day2.js
+++ day_2/day2.js
@@ -14,15 +14,39 @@
 //     console.log("All are equal");
 // }
 
-
 //  ! Check if a Number is Positive, Negative, or Zero
 
-const n = 7;
+// const n = 7;
 
-if (n > 0) {
-    console.log("Positive");
-} else if (n < 0) {
-    console.log("Negative");
+// if (n > 0) {
+//     console.log("Positive");
+// } else if (n < 0) {
+//     console.log("Negative");
+// } else {
+//     console.log("Equal to zero");
+// }
+
+// ! Calculate Electricity Bill
+
+const units = 230;
+
+let remainingUnits = 0;
+let totalBill = 0;
+
+if (units <= 100) {
+	totalBill += units * 5;
+} else if (units <= 200) {
+	totalBill += 100 * 5;
+	totalBill += (units - 100) * 7;
+} else if (units <= 300) {
+	totalBill += 100 * 5;
+	totalBill += 100 * 7;
+	totalBill += (units - 200) * 10;
 } else {
-    console.log("Equal to zero");
+	totalBill += 100 * 5;
+	totalBill += 100 * 7;
+	totalBill += 100 * 10;
+	totalBill += (units - 300) * 12;
 }
+
+console.log(totalBill);
