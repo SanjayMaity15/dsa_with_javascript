f615e17abfdeb571e572948cc9ca8e5a0e9fc327
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-01 09:58:56 +0530

    Check if a Number is Positive, Negative, or Zero

--- day_2/day2.js
+++ day_2/day2.js
@@ -1,15 +1,28 @@
 // ! Find the Maximum of Three Numbers
 
-const n1 = 64,
-	n2 = 4,
-	n3 = 24;
+// const n1 = 64,
+// 	n2 = 4,
+// 	n3 = 24;
 
-if (n1 > n2 && n1 > n3) {
-	console.log("Largest number is ", n1);
-} else if (n2 > n1 && n2 > n3) {
-	console.log("Largest number is ", n2);
-} else if (n3 > n1 && n3 > n2) {
-	console.log("Largest number is ", n3);
+// if (n1 > n2 && n1 > n3) {
+// 	console.log("Largest number is ", n1);
+// } else if (n2 > n1 && n2 > n3) {
+// 	console.log("Largest number is ", n2);
+// } else if (n3 > n1 && n3 > n2) {
+// 	console.log("Largest number is ", n3);
+// } else {
+//     console.log("All are equal");
+// }
+
+
+//  ! Check if a Number is Positive, Negative, or Zero
+
+const n = 7;
+
+if (n > 0) {
+    console.log("Positive");
+} else if (n < 0) {
+    console.log("Negative");
 } else {
-    console.log("All are equal");
+    console.log("Equal to zero");
 }
