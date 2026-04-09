8415244e2d7e4d9a3bdb7a4a96a687ee40be9099
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-09 14:40:23 +0530

    Print Inverted Right-Angled Triangle

--- day_3/day3.js
+++ day_3/day3.js
@@ -1,8 +1,21 @@
 // ! Print Right-Angled Star Triangle
 
+// let row = 5, col = 5;
+
+// for (let x = 1; x <= row; x++){
+//     let str = ""
+//     for (let y = 1; y <= x; y++){
+//         str += "* "
+//     }
+//     console.log(str);
+// }
+
+
+// ! Print Inverted Right-Angled Triangle
+
 let row = 5, col = 5;
 
-for (let x = 1; x <= row; x++){
+for (let x = row; x > 0; x--){
     let str = ""
     for (let y = 1; y <= x; y++){
         str += "* "
