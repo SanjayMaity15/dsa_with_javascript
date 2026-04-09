02c60ac15a4a045e6105b8a3049d6de6bb2b1b7a
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-09 15:03:06 +0530

    Print Pyramid Pattern

--- day_3/day3.js
+++ day_3/day3.js
@@ -10,15 +10,32 @@
 //     console.log(str);
 // }
 
-
 // ! Print Inverted Right-Angled Triangle
 
-let row = 5, col = 5;
+// let row = 5, col = 5;
 
-for (let x = row; x > 0; x--){
-    let str = ""
-    for (let y = 1; y <= x; y++){
-        str += "* "
-    }
-    console.log(str);
+// for (let x = row; x > 0; x--){
+//     let str = ""
+//     for (let y = 1; y <= x; y++){
+//         str += "* "
+//     }
+//     console.log(str);
+// }
+
+// ! Print Pyramid Pattern
+
+let row = 5,
+	col = 5;
+
+for (let x = 1; x <= row; x++) {
+	let line = "";
+	for (let y = 1; y <= row - x; y++) {
+		line += "  ";
+	}
+
+	for (let z = 1; z <= 2 * x - 1; z++) {
+		line += "* ";
+	}
+
+	console.log(line);
 }
