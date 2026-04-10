33f4b678b70c3a9e36505853b1bb5551bf01f8ad
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-10 14:41:07 +0530

    Remove the Decimal Point Mathematically

--- day_4/day4.js
+++ day_4/day4.js
@@ -1,13 +1,24 @@
 // ! Split Number into Digits
 
-let n = 12345;
+// let n = 12345;
 
-let result = []
+// let result = []
 
-while (n !== 0) {
-    let digit = n % 10;
-    result.unshift(digit)
-    n  = Math.floor(n / 10)
-}
+// while (n !== 0) {
+//     let digit = n % 10;
+//     result.unshift(digit)
+//     n  = Math.floor(n / 10)
+// }
 
-console.log(result);
+// console.log(result);
+
+
+// ! Remove the Decimal Point Mathematically
+
+let n = 12.34;
+
+let power = n.toString().split(".")[1].length
+
+console.log(n * Math.pow(10, power));
+
+
