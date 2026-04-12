ba76918024ea937d7b0b80f777a1a33739a2c0f3
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-12 14:09:40 +0530

    Print All Multiples of a Number up to N

--- day_5/day5.js
+++ day_5/day5.js
@@ -1,13 +1,24 @@
 // ! Print All Factors of a Number
 
-let n = 24;
+// let n = 24;
 
-let result = []
+// let result = []
 
-for (let i = 1; i <= n; i++){
-    if (n % i === 0) {
-        result.push(i)
-    }
-}
+// for (let i = 1; i <= n; i++){
+//     if (n % i === 0) {
+//         result.push(i)
+//     }
+// }
 
-console.log(result);
+// console.log(result);
+
+
+// ! Print All Multiples of a Number up to N
+
+// let n = 4, limit = 30;
+
+// for (let i = 4; i <= limit; i += n){
+//     console.log(i);
+// }
+
+
