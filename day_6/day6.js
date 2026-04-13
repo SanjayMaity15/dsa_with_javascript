1c5624b50babd98edc9a7b9b778ee5e6c9a55a89
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-13 17:11:54 +0530

    Check if two Numbers are Co-Prime

--- day_6/day6.js
+++ day_6/day6.js
@@ -1,13 +1,29 @@
 // ! Check if a Number is Prime
 
-let n = 17;
+// let n = 17;
 
-let isPrime = true;
-for (let i = 2; i < n; i++){
-    if (n % i === 0) {
-        isPrime = false
+// let isPrime = true;
+// for (let i = 2; i < n; i++){
+//     if (n % i === 0) {
+//         isPrime = false
+//     }
+// }
+
+// console.log(isPrime);
+
+// ! Check if two Numbers are Co-Prime
+
+let n1 = 7, n2 = 8;
+
+function gcd(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp
     }
+
+    return a
 }
 
-console.log(isPrime);
-
+let result = gcd(n1, n2) === 1 ? "Co-prime" : "not"
+console.log(result);
