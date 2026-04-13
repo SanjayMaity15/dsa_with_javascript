5f38a7aec68805f139e1d1f0e5db07994e2c818b
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-13 17:15:32 +0530

    Print Fibonacci Series up to N Terms

--- day_6/day6.js
+++ day_6/day6.js
@@ -13,17 +13,32 @@
 
 // ! Check if two Numbers are Co-Prime
 
-let n1 = 7, n2 = 8;
+// let n1 = 7, n2 = 8;
 
-function gcd(a, b) {
-    while (b !== 0) {
-        let temp = b;
-        b = a % b;
-        a = temp
-    }
+// function gcd(a, b) {
+//     while (b !== 0) {
+//         let temp = b;
+//         b = a % b;
+//         a = temp
+//     }
 
-    return a
+//     return a
+// }
+
+// let result = gcd(n1, n2) === 1 ? "Co-prime" : "not"
+// console.log(result);
+
+
+// ! Print Fibonacci Series up to N Terms
+
+let n = 10;
+let a = 0;
+let b = 1;
+
+for (let i = 1; i <= n; i++){
+    console.log(a);
+    let temp = a + b;
+    a = b;
+    b = temp;
+
 }
-
-let result = gcd(n1, n2) === 1 ? "Co-prime" : "not"
-console.log(result);
