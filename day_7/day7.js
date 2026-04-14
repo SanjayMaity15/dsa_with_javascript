1a42d5e8d848d43a027b524fa51d569a8213abe1
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-14 20:20:55 +0530

    Find the Product of All Distinct Prime Factors

--- day_7/day7.js
+++ day_7/day7.js
@@ -1,23 +1,46 @@
 //! 1. Find the Prime Factorization (Return as an Array)
 
-let n = 84;
+// let n = 84;
 
-let factors = [];
+// let factors = [];
 
-while (n % 2 === 0) {
-	factors.push(2);
-	n = n / 2;
+// while (n % 2 === 0) {
+// 	factors.push(2);
+// 	n = n / 2;
+// }
+
+// for (let i = 3; i <= Math.sqrt(n); i += 2) {
+// 	while (n % i === 0) {
+// 		factors.push(i);
+// 		n = n / i;
+// 	}
+// }
+
+// if (n > 2) {
+// 	factors.push(n);
+// }
+
+// console.log(factors);
+
+
+// ! Find the Product of All Distinct Prime Factors
+
+let n = 150
+let ans = 1;
+
+
+for (let i = 2; i <= Math.sqrt(n); i++){
+    if (n % i === 0) {
+        ans *= i;
+
+        while (n % i === 0) {
+            n /= i
+        }
+    }
 }
 
-for (let i = 3; i <= Math.sqrt(n); i += 2) {
-	while (n % i === 0) {
-		factors.push(i);
-		n = n / i;
-	}
+if (n > 1) {
+    ans *= n;
 }
 
-if (n > 2) {
-	factors.push(n);
-}
-
-console.log(factors);
+console.log(ans);
