6af2d35f6ed9aeda35ab55663074f3c4988ae4c2
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-17 20:08:48 +0530

    Count Frequency of Each Word in a Sentence

--- day_9/day9.js
+++ day_9/day9.js
@@ -45,30 +45,49 @@
 
 // ! Check if One String is Rotation of Another
 
-let str = "abcde";
-let target = "cdeab";
+// let str = "abcde";
+// let target = "cdeab";
 
-let match;
-let combined = str + str;
+// let match;
+// let combined = str + str;
 
-for (let i = 0; i <= combined.length - target.length; i++) {
-	match = true;
-	for (let j = 0; j < target.length; j++) {
-		if (combined[i + j] !== target[j]) {
-			match = false;
-			break;
-		}
-	}
+// for (let i = 0; i <= combined.length - target.length; i++) {
+// 	match = true;
+// 	for (let j = 0; j < target.length; j++) {
+// 		if (combined[i + j] !== target[j]) {
+// 			match = false;
+// 			break;
+// 		}
+// 	}
 
-	if (match) {
-		console.log(true);
-		break;
-	}
+// 	if (match) {
+// 		console.log(true);
+// 		break;
+// 	}
+// }
+
+
+// // another way
+
+// if (combined.indexOf(target)) {
+//     console.log(true);
+// }
+
+
+// ! Count Frequency of Each Word in a Sentence
+
+let str = "i love coding and i love javascript";
+
+let strArray = str.toLowerCase().split(" ")
+
+let obj = {}
+
+for (let el of strArray) {
+    if (obj.hasOwnProperty(el)) {
+        obj[el] = obj[el]  + 1
+    } else {
+        obj[el] = 1
+    }
 }
 
-
-// another way
-
-if (combined.indexOf(target)) {
-    console.log(true);
-}
+console.log(obj);
