f89fb907cf4692debfd3fef119efcd0688810710
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-17 20:00:14 +0530

    Check if One String is Rotation of Another

--- day_9/day9.js
+++ day_9/day9.js
@@ -23,22 +23,52 @@
 
 // ! Manual Substring Search (Without Using indexOf)
 
-let str = "hello world";
-let target = "wor";
+// let str = "hello world";
+// let target = "wor";
+
+// let match;
+
+// for (let i = 0; i <= str.length - target.length; i++) {
+// 	match = true;
+// 	for (let j = 0; j < target.length; j++) {
+// 		if (str[i + j] !== target[j]) {
+// 			match = false;
+// 			break;
+// 		}
+// 	}
+
+// 	if (match) {
+// 		console.log(i);
+//         break;
+// 	}
+// }
+
+// ! Check if One String is Rotation of Another
+
+let str = "abcde";
+let target = "cdeab";
 
 let match;
+let combined = str + str;
 
-for (let i = 0; i <= str.length - target.length; i++) {
+for (let i = 0; i <= combined.length - target.length; i++) {
 	match = true;
 	for (let j = 0; j < target.length; j++) {
-		if (str[i + j] !== target[j]) {
+		if (combined[i + j] !== target[j]) {
 			match = false;
 			break;
 		}
 	}
 
 	if (match) {
-		console.log(i);
-        break;
+		console.log(true);
+		break;
 	}
+}
+
+
+// another way
+
+if (combined.indexOf(target)) {
+    console.log(true);
 }
