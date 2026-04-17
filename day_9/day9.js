bef95f3a8f99322e5597611953f9730d83a4f02f
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-17 19:40:14 +0530

    Find if One String Is a Substring of Another

---
+++ day_9/day9.js
@@ -0,0 +1,20 @@
+// ! Find if One String Is a Substring of Another
+
+let str = "hellothere";
+let target = "there";
+
+let match;
+
+for (let i = 0; i <= str.length - target.length; i++) {
+	match = true;
+	for (let j = 0; j < target.length; j++) {
+		if (str[i + j] !== target[j]) {
+			match = false;
+			break;
+		}
+	}
+}
+
+let result = match ? true : false;
+
+console.log(result);
