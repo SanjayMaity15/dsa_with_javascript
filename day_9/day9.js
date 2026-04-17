7d56e2fb306ab2ead9f8d9b87a23ab17e9727deb
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-17 19:49:47 +0530

    Manual Substring Search (Without Using indexOf)

--- day_9/day9.js
+++ day_9/day9.js
@@ -1,7 +1,30 @@
 // ! Find if One String Is a Substring of Another
 
-let str = "hellothere";
-let target = "there";
+// let str = "hellottthereee";
+// let target = "there";
+
+// let match;
+
+// for (let i = 0; i <= str.length - target.length; i++) {
+// 	match = true;
+// 	for (let j = 0; j < target.length; j++) {
+// 		if (str[i + j] !== target[j]) {
+// 			match = false;
+// 			break;
+// 		}
+// 	}
+
+// 	if (match) break
+// }
+
+// let result = match ? true : false;
+
+// console.log(result);
+
+// ! Manual Substring Search (Without Using indexOf)
+
+let str = "hello world";
+let target = "wor";
 
 let match;
 
@@ -13,8 +36,9 @@ for (let i = 0; i <= str.length - target.length; i++) {
 			break;
 		}
 	}
+
+	if (match) {
+		console.log(i);
+        break;
+	}
 }
-
-let result = match ? true : false;
-
-console.log(result);
