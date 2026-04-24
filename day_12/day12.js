0abe1da8fb23c9c3df16632b73d6ede5604eb1ff
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-24 20:07:52 +0530

    Sort an Array in Descending Order Using Bubble Sort

--- day_12/day12.js
+++ day_12/day12.js
@@ -1,16 +1,33 @@
 // !  Sort an Array in Ascending Order Using Bubble Sort
 
-let arr = [5, 3, 8, 4, 2];
+// let arr = [5, 3, 8, 4, 2];
 
-for (let i = 0; i < arr.length - 1; i++){
+// for (let i = 0; i < arr.length - 1; i++){
 
-    for (let j = 0; j < arr.length - 1 - i; j++){
-        if (arr[j] > arr[j + 1]) {
-            let temp = arr[j]
-            arr[j] = arr[j + 1]
-            arr[j + 1] = temp
-        }
-    }
+//     for (let j = 0; j < arr.length - 1 - i; j++){
+//         if (arr[j] > arr[j + 1]) {
+//             let temp = arr[j]
+//             arr[j] = arr[j + 1]
+//             arr[j + 1] = temp
+//         }
+//     }
+// }
+
+// console.log(arr);
+
+
+// ! Sort an Array in Descending Order Using Bubble Sort
+
+let arr = [7, 1, 5, 3, 2];
+
+for (let i = 0; i < arr.length - 1; i++) {
+	for (let j = 0; j < arr.length - 1 - i; j++) {
+		if (arr[j] < arr[j + 1]) {
+			let temp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = temp;
+		}
+	}
 }
 
 console.log(arr);
