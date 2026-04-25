d7518c673082d833c98a77e51b7bb2af0b7a23e6
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-25 20:19:31 +0530

    Sort a String Alphabetically Using Bubble Sort Logic

--- day_12/day12.js
+++ day_12/day12.js
@@ -15,19 +15,34 @@
 
 // console.log(arr);
 
-
 // ! Sort an Array in Descending Order Using Bubble Sort
 
-let arr = [7, 1, 5, 3, 2];
+// let arr = [7, 1, 5, 3, 2];
 
-for (let i = 0; i < arr.length - 1; i++) {
-	for (let j = 0; j < arr.length - 1 - i; j++) {
-		if (arr[j] < arr[j + 1]) {
-			let temp = arr[j];
-			arr[j] = arr[j + 1];
-			arr[j + 1] = temp;
+// for (let i = 0; i < arr.length - 1; i++) {
+// 	for (let j = 0; j < arr.length - 1 - i; j++) {
+// 		if (arr[j] < arr[j + 1]) {
+// 			let temp = arr[j];
+// 			arr[j] = arr[j + 1];
+// 			arr[j + 1] = temp;
+// 		}
+// 	}
+// }
+
+// console.log(arr);
+
+// !  Sort a String Alphabetically Using Bubble Sort Logic
+
+let str = "javascript".split("");
+
+for (let i = 0; i < str.length - 1; i++) {
+	for (let j = 0; j < str.length - 1 - i; j++) {
+		if (str[j] > str[j + 1]) {
+			let temp = str[j];
+			str[j] = str[j + 1];
+			str[j + 1] = temp;
 		}
 	}
 }
 
-console.log(arr);
+console.log(str.join(""));
