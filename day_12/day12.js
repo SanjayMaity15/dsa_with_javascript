0a95cda224dcdb3702e84f4659626fc9e338ef4f
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-04-26 20:01:47 +0530

    1️⃣Sort an Array of Objects by Age (Ascending)

--- day_12/day12.js
+++ day_12/day12.js
@@ -33,16 +33,54 @@
 
 // !  Sort a String Alphabetically Using Bubble Sort Logic
 
-let str = "javascript".split("");
+// let str = "javascript".split("");
 
-for (let i = 0; i < str.length - 1; i++) {
-	for (let j = 0; j < str.length - 1 - i; j++) {
-		if (str[j] > str[j + 1]) {
-			let temp = str[j];
-			str[j] = str[j + 1];
-			str[j + 1] = temp;
-		}
+// for (let i = 0; i < str.length - 1; i++) {
+// 	for (let j = 0; j < str.length - 1 - i; j++) {
+// 		if (str[j] > str[j + 1]) {
+// 			let temp = str[j];
+// 			str[j] = str[j + 1];
+// 			str[j + 1] = temp;
+// 		}
+// 	}
+// }
+
+// console.log(str.join(""));
+
+// !  Sort an Array and Count the Number of Swaps Performed
+
+// let arr = [4, 3, 2, 1];
+// let swapCount = 0;
+
+// for (let i = 0; i < arr.length - 1; i++) {
+// 	for (let j = 0; j < arr.length - 1 - i; j++) {
+// 		if (arr[j] > arr[j + 1]) {
+// 			let temp = arr[j];
+// 			arr[j] = arr[j + 1];
+// 			arr[j + 1] = temp;
+// 			swapCount++
+// 		}
+// 	}
+// }
+
+
+// console.log(swapCount);
+
+
+//  ! 1️⃣ Sort an Array of Objects by Age (Ascending)
+
+let arr = [
+	{name: "Sanjay", age: 24},
+	{name: "Anowar", age: 14},
+	{name: "Rahul", age: 42},
+]
+
+arr.sort((a, b) => {
+	if (a.age > b.age) {
+		return 1
+	} else {
+		return -1
 	}
-}
+})
 
-console.log(str.join(""));
+console.log(arr);
