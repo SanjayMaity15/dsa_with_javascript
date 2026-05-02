3be2b7e07fdebc250db438f9e1fcd4334dffe0b0
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-02 10:21:54 +0530

    Given two sorted arrays, find the **kth smallest element** using merge logic (without merging fully).

--- day_14/day14.js
+++ day_14/day14.js
@@ -60,29 +60,81 @@
 
 // Output: [1, 2, 3, 4, 5, 6]
 
-let A = [1, 3, 5];
-let B = [2, 4, 6];
-let i = 0, j = 0;
+// let A = [1, 3, 5];
+// let B = [2, 4, 6];
+// let i = 0, j = 0;
 
-let mergeArr = []
+// let mergeArr = []
+
+// while (i < A.length && j < B.length) {
+//     if (A[i] <= B[j]) {
+//         mergeArr.push(A[i])
+//         i++
+//     } else {
+//         mergeArr.push(B[j])
+//         j++
+//     }
+// }
+
+// while (i < A.length) {
+//     mergeArr.push(A[i])
+//     i++
+// }
+// while (j < B.length) {
+//     mergeArr.push(B[j])
+//     j++
+// }
+
+// console.log(mergeArr);
+
+// ! Given two sorted arrays, find the **kth smallest element** using merge logic (without merging fully).
+
+// A = [1, 3, 7];
+// B = [2, 4, 6];
+// k = 4;
+// Output: 4;
+
+
+let A = [1, 3, 7]
+let B = [2, 4, 6]
+
+let k = 4;
+let i = 0, j = 0;
+let count = 0;
+let current = -1;
 
 while (i < A.length && j < B.length) {
     if (A[i] <= B[j]) {
-        mergeArr.push(A[i])
+        current = A[i]
         i++
+        count++
     } else {
-        mergeArr.push(B[j])
+        current = B[j]
         j++
+        count++
+    }
+
+    if (count === k) {
+        console.log(current);
+        return
     }
 }
 
 while (i < A.length) {
-    mergeArr.push(A[i])
-    i++
+    current = A[i]
+    i++;
+    count++
+    if (count === k) {
+        console.log(current);
+        return
+    }
 }
 while (j < B.length) {
-    mergeArr.push(B[j])
-    j++
+    current = B[j]
+    j++;
+    count++
+    if (count === k) {
+        console.log(current);
+        return
+    }
 }
-
-console.log(mergeArr);
---
+++ day_14/question.md
@@ -0,0 +1,67 @@
+## 1. Sort an Array using Merge Sort assending and decending
+
+👉 **Question:**
+Given an array, sort it using Merge Sort.
+
+**Example:**
+
+```
+Input:  [5, 2, 4, 1, 3]
+Output: [1, 2, 3, 4, 5]
+```
+
+✔ This is the **standard implementation question**.
+
+---
+
+## 2. Count Inversions in an Array (Very Important 🔥)
+
+👉 **Question:**
+Find number of pairs `(i, j)` such that:
+
+```
+i < j and arr[i] > arr[j]
+```
+
+**Example:**
+
+```
+Input:  [2, 4, 1, 3, 5]
+Output: 3
+```
+
+✔ Trick: Solve using Merge Sort while merging.
+
+---
+
+## 3. Merge Two Sorted Arrays
+
+👉 **Question:**
+Given two sorted arrays, merge them into one sorted array.
+
+**Example:**
+
+```
+A = [1, 3, 5]
+B = [2, 4, 6]
+
+Output: [1, 2, 3, 4, 5, 6]
+```
+
+✔ This is the **merge step of merge sort**
+
+
+## 4. Find Kth Element using Merge Process
+
+👉 **Question:**
+Given two sorted arrays, find the **kth smallest element** using merge logic (without merging fully).
+
+**Example:**
+
+```
+A = [1, 3, 7]
+B = [2, 4, 6]
+k = 4
+Output: 4
+```
+
