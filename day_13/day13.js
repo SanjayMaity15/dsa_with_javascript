3a2a77048a593e87f85c6b51c1875c48cd4dbe3e
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-02 06:40:15 +0530

    Sort an Array in Ascending Order Using Selection Sort

---
+++ day_13/day13.js
@@ -0,0 +1,22 @@
+// !  Sort an Array in Ascending Order Using Selection Sort
+
+let arr = [7, 2, 9, 4, 1];
+
+for (let i = 0; i < arr.length - 1; i++){
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++){
+        if (arr[minIndex] > arr[j]) {
+            minIndex = j;
+        }
+    }
+
+    if (minIndex !== i) {
+        let temp = arr[i];
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+    }
+
+}
+
+console.log(arr);
