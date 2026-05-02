fffb0fa03e6201c03e30c374ae5624af16f06093
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-02 06:42:18 +0530

    Sort an Array in Descending Order using selection sort

--- day_13/day13.js
+++ day_13/day13.js
@@ -1,12 +1,36 @@
 // !  Sort an Array in Ascending Order Using Selection Sort
 
-let arr = [7, 2, 9, 4, 1];
+// let arr = [7, 2, 9, 4, 1];
+
+// for (let i = 0; i < arr.length - 1; i++){
+//     let minIndex = i;
+
+//     for (let j = i + 1; j < arr.length; j++){
+//         if (arr[minIndex] > arr[j]) {
+//             minIndex = j;
+//         }
+//     }
+
+//     if (minIndex !== i) {
+//         let temp = arr[i];
+//         arr[i] = arr[minIndex]
+//         arr[minIndex] = temp
+//     }
+
+// }
+
+// console.log(arr);
+
+
+// ! Sort an Array in Descending Order
+let arr =  [3, 8, 5, 2, 9]
+// Output: [9, 8, 5, 3, 2]
 
 for (let i = 0; i < arr.length - 1; i++){
     let minIndex = i;
 
     for (let j = i + 1; j < arr.length; j++){
-        if (arr[minIndex] > arr[j]) {
+        if (arr[minIndex] < arr[j]) {
             minIndex = j;
         }
     }
@@ -20,3 +44,4 @@ for (let i = 0; i < arr.length - 1; i++){
 }
 
 console.log(arr);
+
