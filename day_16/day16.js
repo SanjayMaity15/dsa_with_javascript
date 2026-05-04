dd0669bb1f639bb33971b29afae4b945023cde27
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-04 19:34:02 +0530

    Find the Smallest Element Greater Than a Given Value

--- day_16/day16.js
+++ day_16/day16.js
@@ -59,21 +59,48 @@
 // Input: [2, 4, 4, 4, 9, 11], search = 4
 // Output: index 3
 
-let arr = [2, 4, 4, 4, 9, 11]
-let search = 4;
+// let arr = [2, 4, 4, 4, 9, 11]
+// let search = 4;
+
+// let start = 0;
+// let end = arr.length - 1
+
+// let ans = -1;
+
+// while (start <= end) {
+//     let mid = start + Math.floor((end - start) / 2);
+
+//     if (arr[mid] === search) {
+//         ans = mid;
+//         start = mid + 1
+//     } else if (arr[mid] > search) {
+//         end = mid - 1
+//     } else {
+//         start = mid + 1
+//     }
+
+// }
+
+// console.log(ans);
+
+
+//  ! 1️⃣ Find the Smallest Element Greater Than a Given Value
+// Input: [3, 5, 8, 12, 17], search = 10
+// Output: 12
+
+let arr = [3, 5, 8, 12, 17]
+let search = 10;
 
 let start = 0;
-let end = arr.length - 1
-
-let ans = -1;
+let end = arr.length - 1;
+let ans = 0;
 
 while (start <= end) {
-    let mid = start + Math.floor((end - start) / 2);
+    
+    let mid = start + Math.floor((end - start) / 2)
 
-    if (arr[mid] === search) {
-        ans = mid;
-        start = mid + 1
-    } else if (arr[mid] > search) {
+    if (arr[mid] > search) {
+        ans = arr[mid]
         end = mid - 1
     } else {
         start = mid + 1
