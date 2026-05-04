51880083240e03e1228753d644b0e641e78b96a1
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-04 19:21:32 +0530

    Find the Last Occurrence of a Repeated Number

--- day_16/day16.js
+++ day_16/day16.js
@@ -24,24 +24,55 @@
 // }
 
 // ! 2️⃣ Find the First Occurrence of a Repeated Number
+
 // Input: [2, 4, 4, 4, 9, 11], search = 4
 // Output: index 1
 
-let arr = [2, 7, 3, 4, 4, 4, 9, 11]
+// let arr = [2, 7, 3, 4, 4, 4, 9, 11]
 
+// let search = 4;
+
+// let start = 0;
+// let end = arr.length - 1;
+// let ans = -1;
+
+// while (start <= end) {
+    
+//     let mid = start + Math.floor((end - start) / 2);
+
+//     if (arr[mid] === search) {
+//         ans = mid;
+//         end = mid - 1
+//     } else if (arr[mid] > search) {
+//         end = mid - 1
+//     } else {
+//         start = mid + 1
+//     }
+
+// }
+
+// console.log(ans);
+
+
+//!  Find the Last Occurrence of a Repeated Number
+
+// Input: [2, 4, 4, 4, 9, 11], search = 4
+// Output: index 3
+
+let arr = [2, 4, 4, 4, 9, 11]
 let search = 4;
 
 let start = 0;
-let end = arr.length - 1;
+let end = arr.length - 1
+
 let ans = -1;
 
 while (start <= end) {
-    
     let mid = start + Math.floor((end - start) / 2);
 
     if (arr[mid] === search) {
         ans = mid;
-        end = mid - 1
+        start = mid + 1
     } else if (arr[mid] > search) {
         end = mid - 1
     } else {
