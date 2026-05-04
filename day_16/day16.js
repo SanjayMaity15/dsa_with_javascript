6757988ca2ba0d50143030792963cbd5b1424b30
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-04 19:12:44 +0530

    Find the First Occurrence of a Repeated Number

--- day_16/day16.js
+++ day_16/day16.js
@@ -4,21 +4,50 @@
 // Output: 3
 // Return -1 If Element Is Not Found
 
-let arr = [1, 3, 5, 7, 9];
-let search = 7;
+// let arr = [1, 3, 5, 7, 9];
+// let search = 7;
+
+// let start = 0;
+// let end = arr.length - 1;
+
+// while (start <= end) {
+// 	let mid = Math.floor(start + (end - start) / 2);
+
+// 	if (arr[mid] === search) {
+// 		console.log("Found at index:", mid);
+// 		break;
+// 	} else if (arr[mid] > search) {
+// 		end = mid - 1;
+// 	} else {
+// 		start = mid + 1;
+// 	}
+// }
+
+// ! 2️⃣ Find the First Occurrence of a Repeated Number
+// Input: [2, 4, 4, 4, 9, 11], search = 4
+// Output: index 1
+
+let arr = [2, 7, 3, 4, 4, 4, 9, 11]
+
+let search = 4;
 
 let start = 0;
 let end = arr.length - 1;
+let ans = -1;
 
 while (start <= end) {
-	let mid = Math.floor(start + (end - start) / 2);
+    
+    let mid = start + Math.floor((end - start) / 2);
 
-	if (arr[mid] === search) {
-		console.log("Found at index:", mid);
-		break;
-	} else if (arr[mid] > search) {
-		end = mid - 1;
-	} else {
-		start = mid + 1;
-	}
+    if (arr[mid] === search) {
+        ans = mid;
+        end = mid - 1
+    } else if (arr[mid] > search) {
+        end = mid - 1
+    } else {
+        start = mid + 1
+    }
+
 }
+
+console.log(ans);
