a404a0f648b3e18229519cfc7062753668d9ccf3
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-06-14 10:24:43 +0530

    leetcode question no 35

--- leetcode/script.js
+++ leetcode/script.js
@@ -32,26 +32,54 @@
 
 //  Q - 704
 
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number}
+//  */
+// var search = function(nums, target) {
+//     let left = 0;
+//     let right = nums.length - 1;
+
+//     while(left <= right){
+//         let mid = Math.floor((left + right ) / 2)
+
+//         if(nums[mid] === target){
+//             return mid
+//         }else if(nums[mid] < target){
+//             left = mid + 1
+//         }else{
+//             right = mid - 1
+//         }
+//     }
+//     return - 1
+
+// };
+
+
+//  Q - 35
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
+var searchInsert = function (nums, target) {
     let left = 0;
     let right = nums.length - 1;
+    let mid;
 
-    while(left <= right){
-        let mid = Math.floor((left + right ) / 2)
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2)
 
-        if(nums[mid] === target){
+        if (nums[mid] === target) {
             return mid
-        }else if(nums[mid] < target){
+        } else if (nums[mid] < target) {
             left = mid + 1
-        }else{
+        } else {
             right = mid - 1
         }
-    }
-    return - 1
 
+    }
+    return left
 };
