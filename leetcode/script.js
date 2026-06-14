4a3aa47dd1723f74819048b0fc1fe8a9515fd29b
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-06-14 10:15:37 +0530

    leetcode question no 704

--- leetcode/script.js
+++ leetcode/script.js
@@ -1,31 +1,57 @@
 // Q - 75
 
+// /**
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+// var sortColors = function(nums) {
+//     let low = 0;
+//     let mid = 0;
+//     let high = nums.length - 1;
+
+//     while(mid <= high){
+//         if(nums[mid] === 0){
+//             let temp = nums[mid]
+//             nums[mid] = nums[low]
+//             nums[low] = temp;
+//             low++
+//             mid++
+//         }else if(nums[mid] === 1){
+//             mid++
+//         }else{
+//             let temp = nums[high]
+//             nums[high] = nums[mid]
+//             nums[mid] = temp
+//             high--
+//         }
+//     }
+
+
+//     return nums
+// };
+
+//  Q - 704
+
 /**
  * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {number} target
+ * @return {number}
  */
-var sortColors = function(nums) {
-    let low = 0;
-    let mid = 0;
-    let high = nums.length - 1;
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
 
-    while(mid <= high){
-        if(nums[mid] === 0){
-            let temp = nums[mid]
-            nums[mid] = nums[low]
-            nums[low] = temp;
-            low++
-            mid++
-        }else if(nums[mid] === 1){
-            mid++
+    while(left <= right){
+        let mid = Math.floor((left + right ) / 2)
+
+        if(nums[mid] === target){
+            return mid
+        }else if(nums[mid] < target){
+            left = mid + 1
         }else{
-            let temp = nums[high]
-            nums[high] = nums[mid]
-            nums[mid] = temp
-            high--
+            right = mid - 1
         }
     }
+    return - 1
 
-
-    return nums
 };
